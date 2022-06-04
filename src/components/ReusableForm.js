8bb8f42f86ee1d30188fc7ece7ead106008107e3
Patrick Dolan <dolanp1992@gmail.com>
2022-06-03 20:49:56 -0700

    refactor form to use bootstrap styling, and rows and columns layout

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,32 +1,66 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
 
 function ReusableForm(props) {
   return (
     <React.Fragment>
-      <form onSubmit={props.formSubmissionHandler}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Brew Name" />
-        <input
-          type="text"
-          name="brand"
-          placeholder="Brand Name" />
-        <input
-          type="number"
-          name="price"
-          placeholder="Price" />
-        <input
-          type="number"
-          name="alcoholContent"
-          placeholder="Alcohol Content Percentage" />
-        <input
-          type="number"
-          name="pints"
-          placeholder="Pints" />
-        <button type="submit">{props.buttonText}</button>
-      </form>
+      <Container>
+
+        <Form onSubmit={props.formSubmissionHandler}>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3">
+                <Form.Label>Keg Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="name"
+                  placeholder="Brew Name" />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group className="mb-3">
+                <Form.Label>Brand Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="brand"
+                  placeholder="Brand Name" />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3">
+                <Form.Label>Price Per Pint</Form.Label>
+                <Form.Control
+                  type="number"
+                  name="price"
+                  placeholder="Price" />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group className="mb-3">
+                <Form.Label>Alcohol Content</Form.Label>          
+                <Form.Control
+                  type="number"
+                  name="alcoholContent"
+                  placeholder="Alcohol Content Percentage" />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group className="mb-3">
+                <Form.Label>Pints</Form.Label>
+                <Form.Control
+                  type="number"
+                  name="pints"
+                  placeholder="Pints" />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Button variant="success" className="my-3 w-100" type="submit">{props.buttonText}</Button>
+        </Form>
+      </Container>
+      <hr />
     </React.Fragment>
   );
 }
